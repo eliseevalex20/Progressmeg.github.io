44c917ea47eae76dd82dc1f43cb7776876f6941e
eliseevalex20 <64779288+eliseevalex20@users.noreply.github.com>
2022-05-14 06:33:12 +0300

    First

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.slider_brend').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1
+    })
+    $('.slider_rev').slick({
+        
+        centerMode: true,
+        centerPadding: '300px',
+        slidesToShow: 1
+    })
+})
