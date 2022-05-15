99e933e4f0ba68e7b790fb8ebf5c7a8c7a0fd9df
eliseevalex20 <64779288+eliseevalex20@users.noreply.github.com>
2022-05-16 01:38:11 +0300

    4

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 $(function(){
     $('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
+
   slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
@@ -19,27 +17,23 @@ $(function(){
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: true
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: true
       }
     }
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
   ]
-});
-    
-    
-    
-    
-    
-    
+}),
     
     
     $('.slider_brend').slick({
