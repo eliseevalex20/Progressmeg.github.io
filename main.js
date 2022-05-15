6b671dd871db2daebfe1d1a7987ef2f44ac35254
eliseevalex20 <64779288+eliseevalex20@users.noreply.github.com>
2022-05-15 06:10:15 +0300

    third

--- main.js
+++ main.js
@@ -1,12 +1,89 @@
 $(function(){
+    $('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+    
+    
+    
+    
+    
+    
+    
+    
     $('.slider_brend').slick({
         slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
         slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
     })
     $('.slider_rev').slick({
         
         centerMode: true,
         centerPadding: '320px',
-        slidesToShow: 1
+        slidesToShow: 1,
+        responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        centerPadding: '320px',
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        centerPadding: '320px',
+        slidesToShow: 3,
+      }
+    }
+  ]
     })
 })
